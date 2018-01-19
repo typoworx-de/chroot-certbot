96183462645a9caca016bae8d3d32ef843e2cdb7
root <root@lb1.proxy.de-fr1.typoworx.de>
2018-01-19 15:44:22 +0100

    Updating

--- install.sh
+++ install.sh
@@ -6,5 +6,14 @@ sudo debootstrap xenial $TARGET http://de.archive.ubuntu.com/ubuntu/;
 
 echo '[*] Adding user certbot-maint with ssh/sudo privileges';
 useradd -r certbot-maint -d /etc/letsencrypt -s /bin/bash;
+
+cp -R ./etc/* /etc/;
+cp -R ./usr/ /usr/;
+
+schroot -c chroot-letsencrypt -- sh -c 'apt-get install ssh -y ; ssh-keygen -t rsa -C "" -P "" -f "/root/.ssh/id_rsa.pub" -q';
+
+mkdir -p /etc/letsencrypt/.ssh/;
 cat $TARGET/root/.ssh/id_rsa.pub >> /etc/letsencrypt/.ssh/authorized_keys;
+
+# ToDo use /etc/suders.d/
 echo 'certbot-maint   ALL=NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo;
